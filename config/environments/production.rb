2580e5be5197f20c75babe8a4480d69c3c8d4b91
Dyego Luz <dyegodiscipulo@gmail.com>
2025-09-29 10:50:10 -0300

    Assets removed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-	config.assets.compile = true
+	# config.assets.compile = true
 	config.action_mailer.default_url_options = { host: '.socionerd.com.br' }
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -29,7 +29,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fall back to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  # config.assets.compile = false
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
