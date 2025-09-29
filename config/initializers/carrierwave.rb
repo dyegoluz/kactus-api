2ec556317970cbe85f745d8e44b14e5ae2d19cb0
Dyego Luz <dyegodiscipulo@gmail.com>
2025-09-29 08:28:19 -0300

    feat: carrierwave for production

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,13 +1,12 @@
 CarrierWave.configure do |config|
-  if ENV['AWS_ACCESS_KEY'] && ENV['AWS_SECRET_KEY'] && ENV['AWS_S3_BUCKET']
-    config.fog_credentials = {
-      :provider               => 'AWS',
-      :aws_access_key_id      => ENV['AWS_ACCESS_KEY'],
-      :aws_secret_access_key  => ENV['AWS_SECRET_KEY'],
-      #:region                 => 'sa-east-1',
-      #:host                   => 's3.example.com',
-      #:endpoint               => 'https://s3.example.com:8080'
-    }
-    config.fog_directory  = ENV['AWS_S3_BUCKET']
-  end
+  config.fog_credentials = {
+    :provider               => 'AWS',
+    :aws_access_key_id      => "DO00ZENTHTNMAPUNBDLA",
+    :aws_secret_access_key  => "T2MUNvRWOfjMLTKx1ed5M0QOUNkuwNzOT45lU7uiZww",
+    :region                 => 'nyc3',
+    #:host                   => 's3.example.com',
+    :endpoint               => 'https://nyc3.digitaloceanspaces.com'
+  }
+  config.fog_directory  = "kactushub"
+  config.fog_public    = true
 end
